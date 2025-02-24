44f53f74d63cf1270eca6dedcfe8dc80e8023a5a
6Preniador9 <onlystudysixnine@gmail.com>
2025-02-23 19:37:51 -0500

    Agrega input en lista con función

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let name = document.getElementById('amigo').value.trim();
+    if (name == "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    amigos.push(name);
+    document.getElementById('amigo').value = "";
+    document.getElementById('amigo').focus();
+    console.log(amigos);
+    listaAmigos();
+}
